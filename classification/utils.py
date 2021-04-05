1aede4f02eafc4c7430a3a19abfb1c23e1575ea2
Paula <paulatfortuna@gmail.com>
2021-04-05 09:32:21 +0100

    commit files and plots

---
+++ classification/utils.py
@@ -0,0 +1,24 @@
+
+###############################
+# Design variables
+##############################
+
+#“Arial”, “Balto”, “Courier New”, “Droid Sans”,, “Droid Serif”, “Droid Sans Mono”, “Gravitas One”, “Old Standard TT”,
+# “Open Sans”, “Overpass”, “PT Sans Narrow”, “Raleway”, “Times New Roman”
+font_for_plots = 'Balto'
+
+colors = {
+    'background': '#FCFAFC',
+    'text': '#22262b',
+    'plot_bar': '#41474b',
+    'title': '#e2005a',
+    'table_background': '#FCFAFC',
+    'table_background_header': '#FCFAFC',
+    'table_font_color_header': '#22262b',
+    'map_max_gradient_color': '#e2005a',
+    'map_min_gradient_color': '#FFFFFF',
+}
+
+#“Arial”, “Balto”, “Courier New”, “Droid Sans”,, “Droid Serif”, “Droid Sans Mono”, “Gravitas One”, “Old Standard TT”,
+# “Open Sans”, “Overpass”, “PT Sans Narrow”, “Raleway”, “Times New Roman”
+font_for_plots = 'Balto'
