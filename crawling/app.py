c2566950417efe6dc10076b3bfa13b5fa54b1a29
Paula <paulatfortuna@gmail.com>
2021-04-05 11:59:12 +0100

    final dockerfile corrections

--- crawling/app.py
+++ crawling/app.py
@@ -4,15 +4,15 @@ from NewsKeywordFiltering import NewsKeywordFiltering
 from CasesAnnotation import CasesAnnotation
 
 
-#c = Crawler(1)
-#c.crawling()
+c = Crawler(1)
+c.crawling()
 
-#c = CrawlerPosProcessing()
-#c.pos_process_news()
+c = CrawlerPosProcessing()
+c.pos_process_news()
 
-#c = NewsKeywordFiltering()
-#c.initialize_search_keywords_annotation()
-#c.keywords_annotation()
+c = NewsKeywordFiltering()
+c.initialize_search_keywords_annotation()
+c.keywords_annotation()
 
 c = CasesAnnotation()
 c.cases_annotation()
